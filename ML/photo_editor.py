64c74bcdd68ddb5d75330062d8124a323203240d
chasemc67 <chasemc67@gmail.com>
2022-07-03 13:58:31 -0700

    Add ML stuff

--- ML/photo_editor.py
+++ ML/photo_editor.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
+import pwd
 from PIL import Image
 import os, sys
 
-path = "/Users/davescott/Documents/Personal2021/voxmon_aug2021/pokemon_images_resized_background/"
+path = pwd
 dirs = os.listdir( path )
 
 def resize():
     for item in dirs:
         if os.path.isfile(path+item):
             try:
-                im = Image.open(path+item).convert('RGBA')
+                im = Image.open(path+item)
                 f, e = os.path.splitext(path+item)
-                imResize = im.resize((256,256), Image.ANTIALIAS)
-                new_image = Image.new("RGBA", imResize.size, "WHITE")
-                new_image.paste(imResize, mask = imResize)    
-                new_image.convert('RGB').save(f + '.png', 'JPEG', quality=100)
-
+                print("file", f)
+                imResize = im.resize((1024,1024), Image.ANTIALIAS)
+                imResize.save(f + '.png')
             except:
                 pass
 
